ebb770de9cbf7b829dc4de47fa2fc1c9f9be879c
leon019i <leonlord0@gamil.com>
2024-03-25 21:58:07 +0200

    first commit

---
+++ likes/models.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import User
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.db import models
+
+from storefront.store.models import Product
+
+
+# Create your models here.
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
